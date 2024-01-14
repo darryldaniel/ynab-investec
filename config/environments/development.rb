bf875a9e64c890d30a69bc20aaebd0fc1dfe9832
Darryl Daniel <darrylndaniel@gmail.com>
2024-01-14 14:23:28 +0200

    :lipstick: make sure tailwind styles work properly in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # This flag is important to make sure tailwind works properly in development
+  config.assets.debug = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
